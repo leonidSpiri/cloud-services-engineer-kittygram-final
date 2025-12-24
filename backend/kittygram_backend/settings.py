6d583c9a6d5b74b76a556b7a4f1617bcf52327a9
leonidSpiri <2003.leonid2003@gmail.com>
2025-12-25 00:48:08 +0300

    chore: update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -115,10 +115,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.getenv("STATIC_ROOT", "/backend_static/static")
+MEDIA_ROOT = os.getenv("MEDIA_ROOT", "/media")
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
