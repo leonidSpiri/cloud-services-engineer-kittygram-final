719f18c86dd2d40f81054a835be7f64695e4efab
leonidSpiri <2003.leonid2003@gmail.com>
2025-12-25 00:29:38 +0300

    chore: update docker-compose.production.yml

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -114,11 +114,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.getenv("STATIC_ROOT", "/app/collected_static")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.getenv("MEDIA_ROOT", "/app/media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
